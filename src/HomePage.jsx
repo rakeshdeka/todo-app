0fa7c4945e8f0fd426eb82a980ba703d4ebc161d
Rakesh Deka <58238022+rakeshdeka@users.noreply.github.com>
2023-09-01 22:37:13 +0530

    first commit

---
+++ src/HomePage.jsx
@@ -0,0 +1,42 @@
+import "./homePage.css";
+import { useState } from "react";
+export default function HomePage() {
+    const [values, setValues] = useState([]);
+    const [newValue, setNewValue] = useState("");
+
+    const handleInputChange = (event) => {
+        setNewValue(event.target.value);
+    };
+
+    const handleAddValue = () => {
+        if (newValue !== "") {
+            setValues([...values, newValue]);
+            setNewValue("");
+        }
+    };
+    // console.log(values);
+    // console.log(newValue);
+
+    return (
+        <>
+            <h1 className="heading">TodoList App</h1>
+            <div>
+                <input
+                    type="text"
+                    value={newValue}
+                    onChange={handleInputChange}
+                    placeholder=" add new task..."
+                />
+
+                <button onClick={handleAddValue}>Add</button>
+            </div>
+            <ul>
+                {values.map((value, index) => (
+                    <li className="inputbox" key={index}>
+                        {value}
+                    </li>
+                ))}
+            </ul>
+        </>
+    );
+}
