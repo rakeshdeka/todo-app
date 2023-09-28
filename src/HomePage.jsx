2617b273158f7c477fc8d5a9a3b7d1b1f92a74e5
Rakesh Deka <58238022+rakeshdeka@users.noreply.github.com>
2023-09-28 08:57:39 +0530

    style(homepage)- work on overflowing text so that text grows vertically

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -1,6 +1,7 @@
 
 import { useState } from "react";
-import rmIcon from "./assets/red-x-line-icon.svg"
+import rmIcon from "./assets/delete_black_24dp.svg"
+import editIcon from "./assets/edit_black_24dp.svg"
 export default function HomePage() {
     const [values, setValues] = useState([]);
     const [newValue, setNewValue] = useState("");
@@ -35,21 +36,29 @@ export default function HomePage() {
                     </div>
                     <div className="border w-[95%] h-[90%] border-yellow-300 flex justify-center items-center">
 
-                        <div className="border w-[50%] h-[95%] border-black self-end text-center overflow-auto ">
+                        <div className="border w-[50%] h-[95%] border-black self-end text-center overflow-auto">
                             <p className="border border-black inline-block p-1 sticky top-0 mb-1">Incomplete</p>
                             <div className="flex gap-2 flex-wrap justify-center items-center truncate">
                                 {values.map((value, index) => (
-
-                                    <div key={index} className="flex flex-wrap mb-2 bg-[#DFCCFB] w-[48%] rounded-xl justify-between flex-col overflow-auto">
-
-                                        <div className="h-32 truncate">{value}</div>
-                                        <div className="flex justify-end mb-2"><img src={rmIcon} alt="rmIcon" className="mt-2 w-[10%] h-[30%] mb-4 mr-2 cursor-pointer" onClick={() => handleRemove(index)} /></div>
+                                    <div key={index} className="flex flex-wrap mb-2 bg-[#E2F6D3] shadow-lg rounded-xl justify-between flex-col overflow-auto">
+                                        <div className="flex justify-between border-b border-slate-600">
+                                            <img src={editIcon} alt="editIcon" className=" mt-2 w-[30%] min-h-[30%] mb-4 mr-2 cursor-pointer" />
+                                            <img src={rmIcon} alt="rmIcon" className=" mt-2 w-[30%] min-h-[30%] mb-4 mr-2 cursor-pointer" onClick={() => handleRemove(index)} />
+                                        </div>
+                                        <p className="block  max-w-[200px] align-top text-left p-1" style={{ whiteSpace: 'pre-wrap', overflowWrap: 'break-word' }}>
+                                            {value}
+                                        </p>
                                     </div>
+
+
+
+
+
+
 
                                 ))}
                             </div>
                         </div>
-
                         <div className="border w-[50%] h-[95%] border-black self-end text-center overflow-auto">
                             <p className="border border-black inline-block p-1 sticky top-0 mb-1">Complete</p>
 
