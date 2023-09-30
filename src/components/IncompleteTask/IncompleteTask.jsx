39e5520f92129c0b9e0491d047746af18c1d9c15
Rakesh Deka <58238022+rakeshdeka@users.noreply.github.com>
2023-09-30 21:56:06 +0530

    refector(IncompleteTask)-redesign the card layout

--- src/components/IncompleteTask/IncompleteTask.jsx
+++ src/components/IncompleteTask/IncompleteTask.jsx
@@ -9,14 +9,15 @@ const IncompleteTask = ({ values, handleRemove }) => {
         <>
             <div className="border w-[50%] h-[95%] border-slate-300 self-end text-center overflow-auto">
                 <p className="border border-black inline-block p-1 sticky top-0 mb-1 bg-[#D3BFDB] shadow-2xl">Incomplete</p>
-                <div className="flex gap-2 flex-wrap justify-center items-center truncate ">
+                <div className="flex gap-2 flex-wrap justify-center items-center truncate">
                     {values.map((value, index) => (
-                        <div key={index} className="flex flex-wrap mb-2 bg-[#E2F6D3] shadow-lg rounded-xl justify-between flex-col overflow-auto  cursor-pointer hover:bg-[#cffaab] min-w-[42%] ">
+                        <div key={index} className="flex flex-wrap mb-2 bg-[#E2F6D3] shadow-lg rounded-xl justify-between flex-col overflow-auto cursor-pointer hover:bg-[#cffaab] w-[40%]"
+                        >
                             <div className="flex justify-between border-b border-slate-300">
-                                <FaEdit className="hover:text-[green] mb-2 mt-2" />
-                                <MdDelete className="hover:text-[red] mb-2 mt-2" onClick={() => handleRemove(index)} />
+                                <FaEdit className="hover:text-[green] mb-2 mt-2 ml-1" />
+                                <MdDelete className="hover:text-[red] mb-2 mt-2 mr-1" onClick={() => handleRemove(index)} />
                             </div>
-                            <p className="block  max-w-[200px] align-top text-left p-1 cursor-move" style={{ whiteSpace: 'pre-wrap', overflowWrap: 'break-word' }}>
+                            <p className="block w-full text-start p-1 cursor-move" style={{ whiteSpace: 'pre-wrap', overflowWrap: 'break-word' }}>
                                 {value}
                             </p>
                         </div>
