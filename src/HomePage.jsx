2204681bdad06d54e57feaca9e7e10091343e4bb
Rakesh Deka <58238022+rakeshdeka@users.noreply.github.com>
2023-10-03 20:08:33 +0530

    feat(TodoTask)-worked on the editing note features & title visibility

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -12,10 +12,8 @@ import InProgress from "./components/InProgress/InProgress";
 export default function HomePage() {
     const [values, setValues] = useState([]);
     const [newValue, setNewValue] = useState("");
-    const [title, setTitle] = useState({})
-
-
-
+    // const [title, setTitle] = useState({})
+    const [visibleTitle, setVisibleTitle] = useState(false)
     const handleInputChange = (event) => {
         setNewValue(event.target.value);
     };
@@ -39,33 +37,36 @@ export default function HomePage() {
         newValues.splice(index, 1);
         setValues(newValues);
     };
-    const handleTitle = () => {
+    const handleVisibility = () => {
 
+        setVisibleTitle(true)
     }
+    const closeTitle = () => {
+        setVisibleTitle(!visibleTitle)
+    }
+
 
     return (
         <>
-            <div className=" h-[100vh]">
+            <div className=" h-[100vh]" >
                 {/* header */}
                 <Header />
                 {/* //need div */}
                 {/* first container */}
                 <div className="h-[90%] bg-[#F0F0F0] flex flex-col justify-center items-center">
                     {/* second container */}
-                    <div className="w-[100%] h-[96%] flex flex-col justify-center items-center">
+                    <div className="w-[100%] h-[96%] flex flex-col justify-center items-center ">
                         {/* input box */}
-                        <div className="h-[30%] flex flex-col items-center">
+                        <div className="h-[30%] flex flex-col items-center w-full">
                             <div className="flex rounded-lg bg-[#D4E4ED] shadow-md p-1 border mt-2 min-h-[20%]">
-                                <div className="flex flex-col h-[70%]"> <input className="w-[400px] min-h-[40%] bg-[#D4E4ED] rounded-l-lg outline-none" type="text" value={newValue}
-                                    onChange={handleInputChange} onKeyDown={handleKeyPress} placeholder="title"
-
-                                />
-                                    <textarea className="min-h-[80px] bg-[#D4E4ED] outline-none resize-none" placeholder="enter description" type="text" />
+                                <div className="flex flex-col h-[70%]">
+                                    {visibleTitle && <input className="w-[400px] min-h-[40%] bg-[#D4E4ED] rounded-l-lg outline-none" type="text" placeholder="title" />}
+                                    <textarea className=" bg-[#D4E4ED] w-[400px] outline-none resize-none overflow-hidden h-auto" placeholder="create a note" type="text" value={newValue}
+                                        onChange={handleInputChange} onKeyDown={handleKeyPress} onClick={handleVisibility} onDoubleClick={closeTitle} />
                                 </div>
 
                                 {/* <button className=" rounded-md border border-black text-sm font-semibold text-black px-2 hover:text-slate-600 shadow-lg" onClick={handleAdd}>Add</button> */}
                                 <GrFormAdd className="rounded-r-lg self-center bg-[#D4E4ED] h-[100%] w-8 cursor-pointer" onClick={handleAdd} />
-
 
                             </div>
                             <div className=" mt-1">
--- src/components/TodoTask/TodoTask.jsx
+++ src/components/TodoTask/TodoTask.jsx
@@ -1,19 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { MdDelete } from "react-icons/md"
 import { FaEdit } from "react-icons/fa"
 import Masonry from 'react-masonry-css'
 import "./custom.css"
 import { IoMdColorPalette } from "react-icons/io"
 import { FiMoreVertical } from "react-icons/fi"
+import { AiOutlineCheckCircle } from "react-icons/ai"
 
 
-const TodoTask = ({ values, handleRemove }) => {
+const TodoTask = ({ values, handleRemove, handleAdd }) => {
+    const [iseditable, setIsEditable] = useState(false)
+    const [isIconVisible, setIsIconVisible] = useState(true)
     const breakpoints = {
         default: 2,
         1100: 1,
         700: 1
     }
 
+    const handleEditNote = (index) => {
+        setIsEditable(true);
+    }
+
+    const handleToggleIcon = () => {
+        setIsIconVisible(!isIconVisible)
+    }
 
 
 
@@ -31,16 +41,22 @@ const TodoTask = ({ values, handleRemove }) => {
 
 
                         {values.map((value, index) => (
-                            <div key={index} className=" mb-2 shadow-lg rounded-3xl  overflow-auto cursor-pointer bg-[#D3BFDB] hover:bg-[#dea4f5] w-full "
+                            <div key={index} className=" mb-2 shadow-lg rounded-md  overflow-auto cursor-pointer bg-[#D3BFDB] hover:bg-[#dea4f5] w-full "
 
                             >
 
 
-                                <p className="block w-full text-start p-1 cursor-move" style={{ whiteSpace: 'pre-wrap', overflowWrap: 'break-word' }}>
+                                <p className="block w-full text-start p-2 rounded-t-md focus:no-underline cursor-text outline-none truncate" onClick={handleToggleIcon} contentEditable={iseditable} tabIndex="0" style={{ whiteSpace: 'pre-wrap', overflowWrap: 'break-word' }}>
                                     {value}
                                 </p>
-                                <div className="flex justify-between border-t border-slate-300">
-                                    <FaEdit className="hover:text-[green] mb-1 mt-1 ml-1" />
+                                <div className="flex justify-between">
+                                    <div className='flex'>
+                                        {isIconVisible ? (
+                                            <FaEdit className="hover:text-[red] mb-1 mt-1 ml-1" onClick={() => { handleEditNote(); handleToggleIcon(); }} />
+                                        ) : (<AiOutlineCheckCircle className="hover:text-[green] mb-1 mt-1 ml-1" onClick={() => { handleToggleIcon(); handleAdd(index); }} />
+                                        )}
+                                    </div>
+
                                     <div className='flex'>
                                         <MdDelete className="hover:text-[red] mb-2 mt-1 mr-1" onClick={() => handleRemove(index)} />
                                         <IoMdColorPalette className="hover:text-[orange] mb-1 mt-1 ml-1" />
